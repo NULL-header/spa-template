889d81033816b2a26b7ae3ff37835eee9130a0cc
NULL-header <poaoq.poaoq@gmail.com>
2021-02-15 07:00:44 +0000

    update: install plugin of eslint at jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   extends: [
     "airbnb-typescript",
+    "plugin:jest/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "prettier",
     "prettier/react",
