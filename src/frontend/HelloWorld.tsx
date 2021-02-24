8914a10304e7402deccae57a89fb696bbba596d2
NULL-header <poaoq.poaoq@gmail.com>
2021-02-24 04:16:14 +0000

    update: follow the changing rules

--- src/frontend/HelloWorld.tsx
+++ src/frontend/HelloWorld.tsx
@@ -4,39 +4,8 @@ import { Box, Divider, Heading, Text, Circle, Stack } from "@chakra-ui/react";
 
 const { name, version } = pkg;
 const infos = { name, version };
-export const HelloWorld = () => {
-  return (
-    <Box>
-      <Circle
-        bg="tomato"
-        color="white"
-        justifyContent="flex-start"
-        marginLeft="5"
-        marginRight="5"
-        marginTop="10"
-        maxHeight="20"
-        minHeight="20"
-      >
-        <Heading height="100%" width="100%" padding="3" paddingLeft="10">
-          Hello, World!
-        </Heading>
-      </Circle>
-      <Box justifyContent="flex-end" display="flex" marginTop="10">
-        <Box width="60%" padding="5">
-          <Stack spacing="6">
-            <Heading>Infos</Heading>
-            <Divider />
-            {Object.entries(infos).map(([name, text], i) => (
-              <Info key={i} {...{ name, text }} />
-            ))}
-          </Stack>
-        </Box>
-      </Box>
-    </Box>
-  );
-};
 
-const Info = (props: { name: string; text: string }) => (
+const Info = ({ paramName, text }: { paramName: string; text: string }) => (
   <Box
     display="flex"
     justifyContent="flex-end"
@@ -45,11 +14,41 @@ const Info = (props: { name: string; text: string }) => (
   >
     <Box marginLeft="5">
       <Heading as="h4" size="md">
-        {props.name}:
+        {paramName}:
       </Heading>
     </Box>
     <Box display="flex" justifyContent="flex-end">
-      <Text>{props.text}</Text>
+      <Text>{text}</Text>
+    </Box>
+  </Box>
+);
+
+export const HelloWorld = () => (
+  <Box>
+    <Circle
+      bg="tomato"
+      color="white"
+      justifyContent="flex-start"
+      marginLeft="5"
+      marginRight="5"
+      marginTop="10"
+      maxHeight="20"
+      minHeight="20"
+    >
+      <Heading height="100%" width="100%" padding="3" paddingLeft="10">
+        Hello, World!
+      </Heading>
+    </Circle>
+    <Box justifyContent="flex-end" display="flex" marginTop="10">
+      <Box width="60%" padding="5">
+        <Stack spacing="6">
+          <Heading>Infos</Heading>
+          <Divider />
+          {Object.entries(infos).map(([paramName, text]) => (
+            <Info key={paramName} {...{ paramName, text }} />
+          ))}
+        </Stack>
+      </Box>
     </Box>
   </Box>
 );
